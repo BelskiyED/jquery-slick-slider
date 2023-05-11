b99c30ae7020afc2b7a36da6a15f2681fca6ff8b
Evgeniy-D-B <Evgeniy_Belskiy95@mail.ru>
2023-05-11 15:24:15 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        adaptiveHeight:true,
+        autoplay:true,
+        arrows:false,
+        speed: 800,
+        autoplayspeed: 2500,
+
+    });
+});
